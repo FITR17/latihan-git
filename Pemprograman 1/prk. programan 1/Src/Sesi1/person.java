61a9db85c8528c8d219ad71a3a3f937cc1df5889
unknown <User_38@.(none)>
2012-10-02 11:25:09 +0700

    fitri

---
+++ Pemprograman 1/prk. programan 1/Src/Sesi1/person.java
@@ -0,0 +1,14 @@
+//Nama : Fitria ismanato
+//Nim` : 4511210012
+//prk pemprograman 4 - 1
+
+public  class person {
+public static void main (String[] args) {
+String nim = "4511210012";
+String nama ;
+nama = "Fitria ismanto";
+System.out.println("Nama saya adalah:" + nama);
+System.out.println("Nim: " + nama);
+}
+}
+
